8e1cb04c077c35c7e2f52474738a8e5065575d04
Elena <elenarosamail@gmail.com>
2023-07-02 08:50:33 -0700

    add if else statements

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,27 @@ function process(input) {
     let myNum = document.getElementById("userAnswers");
     questionForm.addEventlistener("submit", submitForm);
     let myArray = [];
-
+    for (let i = 0; i <= myNum; i++)
+        myArray.push(processNumber(i));
 }
-function processNumber() {
-    let
+function processNumber(num) {
+    let numString = num.toString()
+
+    if (numString.includes("3")) {
+        return "Won't you be my neighbor?";
+    }
+    else if (numString.includes("2")) {
+        return "Beep!";
+    }
+    else if (numString.include("1")) {
+        return "Boop!";
+    }
+    else {
+        return numString;
+    }
+
+    function submitForm() {
+        process("number");
         event.preventDefault();
+    }
 
-
-    function 
