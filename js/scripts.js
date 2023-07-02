c911bce9e701edb6bfaa68f6ba696f2e2752cab6
Elena <elenarosamail@gmail.com>
2023-07-01 23:55:53 -0700

    WIP gathering js form

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,12 @@
-window.onload = function () {
-    const questionForm = document.getElementById("userAnswers");
+function process(input) {
+    let myNum = document.getElementById("userAnswers");
     questionForm.addEventlistener("submit", submitForm);
     let myArray = [];
 
 }
-function submitForm() {
-    event.preventDefault();
-    const gameAnswers = document.getElementById("gameAnswers");
-    gameAnswers.classList.remove("hidden");
-}
+function processNumber() {
+    let
+        event.preventDefault();
 
 
-const numberArray = [1, 2, 3, 3, 7, 18, 63];
-
-if (answer1.value == "apples" && answer2.value == "tea" && answer3.value == "y" && answer4.value == "y" && answer5.value == "frontend") {
-    language.innerHTML = "Possibly JavaScript!";
-}
-else if (answer1.value == "oranges" && answer2.value == "coffee" && answer3.value == "no" && answer4.value == "no" && answer5.value == "backend") {
-    language.innerHTML = "We recommend C#!";
-}
-else if (answer1.value == "apples" && answer2.value == "coffee" && answer3.value == "no" && answer4.value == "no" && answer5.value == "frontend") {
-    language.innerHTML = "Maybe Python is a good fit!";
-}
-
-else {
-    language.innerHTML = "Maybe try piano lessons!";
-}
-
+    function 
